889418f76ceb157af65573ac2ab0dc67cf315d11
SAITOU Keita <keita44.f4@gmail.com>
2026-05-31 03:04:19 +0900

    Add: アカウントロックのrspecを追加

---
+++ spec/system/user_account_lock_spec.rb
@@ -0,0 +1,59 @@
+require "rails_helper"
+
+RSpec.describe "User account lock and unlock" do
+  let(:user) { create(:user, email: "user@example.com") }
+
+  before do
+    # テスト前にメールキューをクリアしておく
+    ActionMailer::Base.deliveries.clear
+
+    # ユーザーがパスワードを複数回間違えてロックされる
+    Devise.maximum_attempts.times {
+      visit new_user_session_path
+      fill_in("user_email", with: user.email)
+      fill_in("user_password", with: "wrong_password")
+      click_button("commit")
+    }
+  end
+
+  it "sends an unlock email to the user" do
+    expect(ActionMailer::Base.deliveries.last.to).to include(user.email)
+  end
+
+  it "shows a error flash message" do
+    expect(page).to have_content(I18n.t("devise.failure.locked"))
+  end
+
+  context "when the user visits the unlock link from the email" do
+    before do
+      unlock_url = ActionMailer::Base.deliveries.last.body.encoded[%r{/users/unlock\?unlock_token=[^"\s<]+}]
+      visit(unlock_url)
+    end
+
+    it "unlocks and allows sign-in with the correct password" do
+      # 正しいパスワードでログイン
+      visit new_user_session_path
+      fill_in("user_email", with: user.email)
+      fill_in("user_password", with: user.password)
+      click_button("commit")
+
+      # ログアウトボタンが表示されていることでログインを確認
+      expect(page).to have_selector(:test_id, "sign_out")
+    end
+  end
+
+  context "after 1 hour from account lock" do
+    it "unlocks and allows sign-in with the correct password" do
+      travel(1.hour + 1.minute) do
+        # 正しいパスワードでログイン
+        visit new_user_session_path
+        fill_in("user_email", with: user.email)
+        fill_in("user_password", with: user.password)
+        click_button("commit")
+
+        # ログアウトボタンが表示されていることでログインを確認
+        expect(page).to have_selector(:test_id, "sign_out")
+      end
+    end
+  end
+end
