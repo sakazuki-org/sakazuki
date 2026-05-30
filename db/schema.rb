2d5b393e076cda6f7b56f97fa49dbf75211c82ae
SAITOU Keita <keita44.f4@gmail.com>
2026-05-30 15:53:25 +0900

    Update: rails 8.1 の db:migrate で schema 更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,91 +10,90 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_09_094424) do
+ActiveRecord::Schema[8.1].define(version: 2024_09_09_094424) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
   end
 
   create_table "photos", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "image"
     t.integer "sake_id"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "sakes", force: :cascade do |t|
-    t.string "name"
-    t.string "kura"
-    t.date "bindume_on"
-    t.date "brewery_year"
-    t.string "todofuken"
-    t.integer "taste_value"
-    t.integer "aroma_value"
-    t.float "nihonshudo"
-    t.float "sando"
-    t.text "aroma_impression"
-    t.string "color"
-    t.text "taste_impression"
-    t.string "nigori"
-    t.string "awa"
-    t.integer "tokutei_meisho", default: 0
-    t.string "genryomai"
-    t.string "kakemai"
-    t.string "kobo"
     t.float "alcohol"
     t.float "aminosando"
-    t.string "season"
-    t.integer "warimizu", default: 0
-    t.integer "moto", default: 0
-    t.integer "seimai_buai"
-    t.string "roka"
-    t.string "shibori"
-    t.text "note"
+    t.text "aroma_impression"
+    t.integer "aroma_value"
+    t.string "awa"
+    t.date "bindume_on"
     t.integer "bottle_level", default: 0
-    t.integer "hiire", default: 0
-    t.integer "price"
-    t.integer "size", default: 720
+    t.date "brewery_year"
+    t.string "color"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "opened_at", null: false
     t.datetime "emptied_at", null: false
+    t.string "genryomai"
+    t.integer "hiire", default: 0
+    t.string "kakemai"
+    t.string "kobo"
+    t.string "kura"
+    t.integer "moto", default: 0
+    t.string "name"
+    t.string "nigori"
+    t.float "nihonshudo"
+    t.text "note"
+    t.datetime "opened_at", null: false
+    t.integer "price"
     t.integer "rating", default: 0, null: false
+    t.string "roka"
+    t.float "sando"
+    t.string "season"
+    t.integer "seimai_buai"
+    t.string "shibori"
+    t.integer "size", default: 720
+    t.text "taste_impression"
+    t.integer "taste_value"
+    t.string "todofuken"
+    t.integer "tokutei_meisho", default: 0
+    t.datetime "updated_at", null: false
+    t.integer "warimizu", default: 0
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: nil
-    t.datetime "remember_created_at", precision: nil
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at", precision: nil
-    t.datetime "last_sign_in_at", precision: nil
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at", precision: nil
     t.boolean "admin", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "confirmation_sent_at", precision: nil
     t.string "confirmation_token"
     t.datetime "confirmed_at", precision: nil
-    t.datetime "confirmation_sent_at", precision: nil
-    t.string "unconfirmed_email"
-    t.string "invitation_token"
-    t.datetime "invitation_created_at", precision: nil
-    t.datetime "invitation_sent_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "current_sign_in_at", precision: nil
+    t.string "current_sign_in_ip"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.integer "failed_attempts", default: 0, null: false
     t.datetime "invitation_accepted_at", precision: nil
+    t.datetime "invitation_created_at", precision: nil
     t.integer "invitation_limit"
+    t.datetime "invitation_sent_at", precision: nil
+    t.string "invitation_token"
     t.integer "invited_by_id"
     t.string "invited_by_type"
+    t.datetime "last_sign_in_at", precision: nil
+    t.string "last_sign_in_ip"
+    t.datetime "locked_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "reset_password_sent_at", precision: nil
+    t.string "reset_password_token"
+    t.integer "sign_in_count", default: 0, null: false
+    t.string "unconfirmed_email"
+    t.string "unlock_token"
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
-
 end
