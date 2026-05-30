b3f76ac8fd370989f1890219de68cd078df27803
SAITOU Keita <keita44.f4@gmail.com>
2026-05-30 16:32:11 +0900

    Remove: メアド確認メール再送機能を無効化
    routeで404にした

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # devise 確認メールの再送機能を無効化する
+  # GET /users/confirmation/new, 確認画面
+  # POST /users/confirmation 確認メール再送リクエスト
+  not_found = ->(_env) { raise(ActionController::RoutingError, "Not Found") }
+  get "users/confirmation/new", to: not_found
+  post "users/confirmation", to: not_found
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { registrations: "users/registrations", invitations: "users/invitations" }
   root "sakes#index"
